562cb7ccae318b60f3442d498502693ea22d9464
王子韬 <wangzitao@xiaomalixing.com>
2020-07-19 16:49:35 +0800

    嵌套滑动：优化打印内容

--- app/src/main/java/com/kyrie/proj/blog/nestedscroll/HorizonRecycleView.kt
+++ app/src/main/java/com/kyrie/proj/blog/nestedscroll/HorizonRecycleView.kt
@@ -15,17 +15,23 @@ class HorizonRecycleView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RecyclerView(context, attrs, defStyleAttr) {
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        Log.i("kyrie", "[HorizonRecycleView][dispatchTouchEvent] ev = $ev")
-        return super.dispatchTouchEvent(ev)
+        Log.i("wzt", "[HorizonRecycleView][dispatchTouchEvent] ev = ${MotionEvent.actionToString(ev!!.action)}")
+        val result = super.dispatchTouchEvent(ev)
+        Log.i("wzt", "[HorizonRecycleView][dispatchTouchEvent] return $result")
+        return result
     }
 
     override fun onTouchEvent(e: MotionEvent?): Boolean {
-        Log.i("kyrie", "[HorizonRecycleView][onTouchEvent] e = $e")
-        return super.onTouchEvent(e)
+        Log.i("wzt", "[HorizonRecycleView][onTouchEvent] e = = ${MotionEvent.actionToString(e!!.action)}")
+        val result = super.onTouchEvent(e)
+        Log.i("wzt", "[HorizonRecycleView][onTouchEvent] return $result")
+        return result
     }
 
     override fun onInterceptTouchEvent(e: MotionEvent?): Boolean {
-        Log.i("kyrie", "[HorizonRecycleView][onInterceptTouchEvent] e = $e")
-        return super.onInterceptTouchEvent(e)
+        Log.i("wzt", "[HorizonRecycleView][onInterceptTouchEvent] e = = ${MotionEvent.actionToString(e!!.action)}")
+        val result = super.onInterceptTouchEvent(e)
+        Log.i("wzt", "[HorizonRecycleView][onInterceptTouchEvent] return $result")
+        return result
     }
 }
--- app/src/main/java/com/kyrie/proj/blog/nestedscroll/VerticalRecycleView.kt
+++ app/src/main/java/com/kyrie/proj/blog/nestedscroll/VerticalRecycleView.kt
@@ -15,17 +15,23 @@ class VerticalRecycleView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RecyclerView(context, attrs, defStyleAttr) {
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        Log.i("kyrie", "[VerticalRecycleView][dispatchTouchEvent] ev = $ev")
-        return super.dispatchTouchEvent(ev)
+        Log.i("wzt", "[VerticalRecycleView][dispatchTouchEvent] ev = ${MotionEvent.actionToString(ev!!.action)}")
+        val result = super.dispatchTouchEvent(ev)
+        Log.i("wzt", "[VerticalRecycleView][dispatchTouchEvent] return $result")
+        return result
     }
 
     override fun onTouchEvent(e: MotionEvent?): Boolean {
-        Log.i("kyrie", "[VerticalRecycleView][onTouchEvent] e = $e")
-        return super.onTouchEvent(e)
+        Log.i("wzt", "[VerticalRecycleView][onTouchEvent] e = = ${MotionEvent.actionToString(e!!.action)}")
+        val result = super.onTouchEvent(e)
+        Log.i("wzt", "[VerticalRecycleView][onTouchEvent] return $result")
+        return result
     }
 
     override fun onInterceptTouchEvent(e: MotionEvent?): Boolean {
-        Log.i("kyrie", "[VerticalRecycleView][onInterceptTouchEvent] e = $e")
-        return super.onInterceptTouchEvent(e)
+        Log.i("wzt", "[VerticalRecycleView][onInterceptTouchEvent] e = = ${MotionEvent.actionToString(e!!.action)}")
+        val result = super.onInterceptTouchEvent(e)
+        Log.i("wzt", "[VerticalRecycleView][onInterceptTouchEvent] return $result")
+        return result
     }
 }
